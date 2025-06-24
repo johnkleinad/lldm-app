eb3ac1509cedf5e6e28ec6ea25f04d937965a164
John Kleinad <jmejia.dev@systemcardinal.com>
2025-06-24 00:57:56 -0600

    fixed useless import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,3 @@
-import { Amplify } from 'aws-amplify';
-import amplifyConfig from './amplify_outputs.json';
-Amplify.configure(amplifyConfig);
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
